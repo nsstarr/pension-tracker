f7985b86df9723597b635bd7e0cc7e76e75aca95
Anastasia Starostina <anastasia.st.stern@gmail.com>
2024-09-26 20:49:09 +0100

    fix: increase page padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const [pensionData, setPensionData] = useState<PensionData | null>(null);
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-5 p-4 md:gap-10 lg:flex-row">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-5 p-10 md:gap-10 lg:flex-row">
       <section className="rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800">
         <h1 className="mb-8 text-2xl font-bold">Pension Tracker</h1>
         <PensionForm onSubmit={setPensionData} />
